0cbefecb7cee427149d538d84e0e60efc7b7375d
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2023-10-23 17:03:45 +0100

    add cog template

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cog-replicate-llama-awq
-How to push any AWQ LLM to Replicate
+# How to push any AWQ LLM to Replicate
+
+1. Download a AWQ model from HuggingFace and place it in /models
+2. Update the `<model_name>` in predict.py to match the file in /models
+3. Create a model on Replicate (https://replicate.com/docs/guides/push-a-transformers-model)
+4. Run `cog login`
+5. Run `cog push r8.im/<your-username>/<your-model-name>`
+6. `docker system prune` to cleanup temp images
---
+++ predict.py
@@ -0,0 +1,55 @@
+import time
+from vllm import LLM, SamplingParams
+import torch
+from cog import BasePredictor, Input
+
+class Predictor(BasePredictor):
+
+    def setup(self):
+        self.llm = LLM(
+            model="./models/<model_name>",
+            quantization="awq",
+            dtype="half"
+        )
+
+    def predict(
+        self,
+        prompt: str = Input(description=f"Text prompt to send to the model."),
+        max_new_tokens: int = Input(
+            description="The maximum number of tokens the model should generate as output.",
+            default=128,
+        ),
+        temperature: float = Input(
+            description="The value used to modulate the next token probabilities.",
+            ge=0.01,
+            le=5,
+            default=0.8,
+        ),
+        top_p: float = Input(
+            description="A probability threshold for generating the output. If < 1.0, only keep the top tokens with cumulative probability >= top_p (nucleus filtering). Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751).",
+            ge=0.01,
+            le=1.0,
+            default=0.95
+        ),
+        top_k: int = Input(
+            description="The number of highest probability tokens to consider for generating the output. If > 0, only keep the top k tokens with highest probability (top-k filtering).",
+            default=50,
+        ),
+        presence_penalty: float = Input(
+            description="Presence penalty",
+            ge=0.01,
+            le=5,
+            default=1.0
+        ),
+    ) -> str:
+        sampling_params = SamplingParams(
+            max_tokens=max_new_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            presence_penalty=presence_penalty
+        )
+        start = time.time()
+        outputs = self.llm.generate([prompt], sampling_params)
+        print(f"\nGenerated {len(outputs[0].outputs[0].token_ids)} tokens in {time.time() - start} seconds.")
+        return outputs[0].outputs[0].text
